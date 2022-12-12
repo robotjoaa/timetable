96d2ae6d26d10df04779df4fe170cdd684da59fc
robotjoaa <36977804+robotjoaa@users.noreply.github.com>
2022-12-12 15:04:50 +0000

    calendar

---
+++ src/calendar.js
@@ -0,0 +1,55 @@
+function createCalendar(){
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    document.getElementById('body').appendChild(table);
+    let dayOfWeek = "일월화수목금토";
+    thead.appendChild(document.createElement('tr'));
+    for(let d=0; d<7; d++){
+        let th_tmp = document.createElement('th');
+        th_tmp.innerHTML = "<p>"+dayOfWeek[d]+"</p>";
+        thead.append(th_tmp);
+    }
+
+    let year = 2023;
+    let month = 12;
+    let startDate = new Date(year,month-1,1,0,1);
+    if(month == 12){
+        year += 1;
+        month = 1;
+    }
+    else{
+        month += 1;
+    }
+    let endDate = new Date(year,month-1,1,0,0);
+    endDate.setDate(endDate.getDate()-1);
+    let startDayOffset = startDate.getDay();
+    let dateNum = endDate.getDate();
+    let weekNum = Math.ceil((startDayOffset -1 + dateNum)/7);
+    let lastDayOffset = (startDayOffset + dateNum) % 7;
+    console.log(lastDayOffset)
+    let num = 1; 
+    for(let j = 0; j < weekNum; j++){
+        let tr_tmp = document.createElement('tr');
+        tbody.append(tr_tmp);
+        for(let i = 0; i < 7; i++){
+            let td_tmp = document.createElement('td');
+            tr_tmp.appendChild(td_tmp);
+            if(j == 0 && i < startDayOffset){
+                continue;
+            }
+            
+            if(j == weekNum-1 && num > dateNum){
+                continue;
+            }
+            td_tmp.innerHTML = "<p>"+num+"</p>";
+            num+=1;
+        }
+    }
+}
+
+createCalendar()
