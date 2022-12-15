03798b9a5d9923284950840ff60fdc6873e47f7e
robotjoaa <36977804+robotjoaa@users.noreply.github.com>
2022-12-15 14:57:02 +0000

    fix

--- src/calendar.js
+++ src/calendar.js
@@ -11,11 +11,12 @@ function createCalendar(){
     thead.appendChild(document.createElement('tr'));
     for(let d=0; d<7; d++){
         let th_tmp = document.createElement('th');
-        th_tmp.innerHTML = "<p>"+dayOfWeek[d]+"</p>";
+        //th_tmp.innerHTML = "<p>"+dayOfWeek[d]+"</p>";
+        th_tmp.innerHTML = dayOfWeek[d];
         thead.append(th_tmp);
     }
 
-    let year = 2023;
+    let year = 2022;
     let month = 12;
     let startDate = new Date(year,month-1,1,0,1);
     if(month == 12){
@@ -30,8 +31,6 @@ function createCalendar(){
     let startDayOffset = startDate.getDay();
     let dateNum = endDate.getDate();
     let weekNum = Math.ceil((startDayOffset -1 + dateNum)/7);
-    let lastDayOffset = (startDayOffset + dateNum) % 7;
-    console.log(lastDayOffset)
     let num = 1; 
     for(let j = 0; j < weekNum; j++){
         let tr_tmp = document.createElement('tr');
@@ -43,11 +42,17 @@ function createCalendar(){
                 continue;
             }
             
-            if(j == weekNum-1 && num > dateNum){
-                continue;
-            }
-            td_tmp.innerHTML = "<p>"+num+"</p>";
+            if(num > dateNum) continue;
+
+            td_tmp.innerHTML = num;
             num+=1;
+        }
+
+        tr_tmp = document.createElement('tr');
+        tbody.append(tr_tmp);
+        for(let i = 0; i < 7; i++){
+            let td_tmp = document.createElement('td');
+            tr_tmp.appendChild(td_tmp);
         }
     }
 }
