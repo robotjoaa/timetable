428092ae450a0c7243e64515d67d6c834ac855eb
robotjoaa <robotjoaa1@naver.com>
2022-12-25 14:52:54 +0000

    Update 4 files

--- src/genetic.js
+++ src/genetic.js
@@ -9,30 +9,80 @@
 4. crossover
 
 5. mutation
-*/ 
+*/
 
 /*
-    월요일 부터 시작
+    일요일 부터 시작
     0    1   2   3    4   5    6    7   8    9    10
-    월야 화야 수야 목야 금야 토주1 토주2 토야 일주1 일주2 일야
+    일주1 일주2 일야 월야 화야 수야 목야 금야 토주1 토주2 토야
     
 */
-const 
+var year = 2022;
+var month = 12;
+const SHIFT_NUM = [[0, 1, 2], [3], [4], [5], [6], [7], [8, 9, 10]];
 
-class Chromosome{
-    constructor(len){
-        this.len = len;
-        this.value = 
-    }
-    
-    function init(){
-        
-    }
+function makeShiftArr(year, month) {
+  let startDate = new Date(year, month - 1, 1);
+
+  if (month == 12) {
+    year += 1;
+    month = 1;
+  } else {
+    month += 1;
+  }
+
+  let endDate = new Date(year, month - 1, 1);
+  endDate.setDate(endDate.getDate() - 1);
+  let startDay = startDate.getDay();
+  let dateNum = endDate.getDate();
+
+  let currDay = startDay;
+  let result = [];
+  for (let i = 0; i < dateNum; i++) {
+    result.push(...SHIFT_NUM[currDay]);
+    currDay++;
+    if (currDay >= 7) currDay = 0;
+  }
+  return result;
 }
 
+const SHIFT_MASK = makeShiftArr(year, month);
+const SHIFT_LEN = SHIFT_MASK.length;
 
-class GeneticAlgorithm{
-    constructor(){
-        
+class Chromosome {
+  constructor(len) {
+    this.len = len;
+    this.value = Array(len).fill(0);
+  }
+
+  init() {
+    let prob = 0.5;
+    function rndFunc() {
+      return Math.random() >= prob ? 1 : 0;
     }
+    this.value = this.value.map(rndFunc);
+  }
 }
+
+function getWorkerInfo(jsonFile) {
+  let workerData = JSON.parse(JSON.stringify(WorkerInfo));
+  return workerData;
+}
+
+class Worker {
+  constructor(name) {
+    this.name = name;
+    this.chromosome = new Chromosome(SHIFT_LEN);
+    this.constraints = { hard: [], soft: [] };
+  }
+}
+
+class GeneticAlgorithm {
+  constructor() {}
+
+  initialize() {
+    this.workers = getWorkerInfo();
+  }
+}
+
+document.writeln(getWorkerInfo()["현민"]);
